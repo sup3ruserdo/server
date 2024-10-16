5f10aa59210931b868bdc701d1c797c9ee7f28fc
sup3ruserdo <Aleksandrastrelchuk62@gmail.com>
2024-10-16 23:02:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,26 @@
 self.addEventListener('fetch', event => {
     const request = event.request;
 
+    // Intercept HEAD requests
     if (request.method === 'HEAD') {
-        event.respondWith(new Response(null, {
-            status: 200,
-            headers: {
-                'Content-Type': 'text/x-component',
-            }
-        }));
-    } else if (request.method === 'GET') {
-        event.respondWith(new Response(null, {
-            status: 302,
-            headers: {
-                'Location': 'https://example.com',
-            }
-        }));
+        event.respondWith(
+            new Response(null, {
+                status: 200,
+                headers: {
+                    'Content-Type': 'text/x-component',
+                }
+            })
+        );
+    }
+    // Intercept GET requests and redirect
+    else if (request.method === 'GET') {
+        event.respondWith(
+            new Response(null, {
+                status: 302,
+                headers: {
+                    'Location': 'https://example.com',
+                }
+            })
+        );
     }
 });
