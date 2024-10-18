7559ffc24c83aa757bc61f1d74211b39de974da4
sup3ruserdo <Aleksandrastrelchuk62@gmail.com>
2024-10-18 22:03:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def catch(path):
         resp = Response("")
         resp.headers['Content-Type'] = 'text/x-component'
         return resp
-    return redirect('http://0.0.0.0:3000/home?token=3c7325daa4bada844ce056e1e914c448&directory={{7*7}}')
+    return redirect('http://0.0.0.0:3000/home?token=3c7325daa4bada844ce056e1e914c448&directory=/{{7*7}}')
